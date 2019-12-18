b39e9df267b2ad59f4d2933f4dbcfd765bbf5f5b
myriam33 <mar.iam3@live.fr>
2019-12-18 11:19:49 +0100

    bonjour

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# tp
+<h1>TP Architecture</h1>
+<p>Voici le fichier PHP permettant de gérer la pagination d'une liste de commentaire.</p>
+<p>Veuillez cloner mon dépot et déposer sur le votre (en ligne) une version corrigée du tp.<br>
+Vous prendrez en charge les extrémités (première page et dernière page) dans l'affichage des numéros de pages.</p>
---
+++ commentaires.php
@@ -0,0 +1,73 @@
+<!doctype html> 
+<html>
+	<head> 
+		<meta charset="utf-8">
+	</head> 
+	<body>
+		<form method="POST">
+			<input type="text" name="nom" placeholder="Nom"><br>
+			<textarea name="contenu" placeholder="Commentaire"></textarea><br>
+			<input type="submit" value="Envoyer" name="envoyer">
+		</form>
+		<?php
+			$lien=mysqli_connect("localhost","root","root","tp");
+			if(isset($_POST['envoyer']))
+			{
+				$nom=trim(htmlentities(mysqli_real_escape_string($lien,$_POST['nom'])));
+				$contenu=trim(htmlentities(mysqli_real_escape_string($lien,$_POST['contenu'])));
+				$req="INSERT INTO comment VALUES (NULL,'$nom','$contenu')";
+				$res=mysqli_query($lien,$req);
+				if(!$res)
+				{
+					echo "Erreur SQL:$req<br>".mysqli_error($lien);
+				}
+			}
+			
+			if(!isset($_GET['page']))
+			{
+				$page=1;
+			}
+			else
+			{
+				$page=$_GET['page'];
+			}
+			$commparpage=3;
+			$premiercomm=$commparpage*($page-1);
+			$req="SELECT * FROM comment ORDER BY id LIMIT $premiercomm,$commparpage";/* LIMIT dit ou je commence et combien j'en prends*/
+			$res=mysqli_query($lien,$req);
+			if(!$res)
+			{
+				echo "erreur SQL:$req<br>".mysqli_error($lien);
+			}
+			else
+			{
+				while($tableau=mysqli_fetch_array($res))
+				{
+					echo "<h2>".$tableau['nom']."</h2>";
+					echo "<p>".$tableau['contenu']."</p>";
+				}
+			}
+			
+			$req="SELECT * FROM comment";
+			$res=mysqli_query($lien,$req);
+			if(!$res)
+			{
+				echo "Erreur SQL:$req<br>".mysqli_error($lien);
+			}
+			else
+			{
+				$nbcomm=mysqli_num_rows($res); // Retourne le nombre de lignes dans un résultat. 
+				$nbpages=ceil($nbcomm/$commparpage); /*Ceil arrondit a l'entier supérieur*/
+				echo "<br> Pages : ";
+				echo "<a href='commentaires.php?page=1'> << </a>";
+				echo "<a href='commentaires.php?page=".($page-1)."'> < </a>";
+				for($i=($page-2);$i<=($page+2);$i++)
+				{
+					echo "<a href='commentaires.php?page=$i'> $i </a>";
+				}
+			}
+			echo "<a href='commentaires.php?page=".($page+1)."'> > </a>";
+			echo "<a href='commentaires.php?page=$nbpages'> >> </a>";
+			
+			mysqli_close($lien);
+		?>																			
